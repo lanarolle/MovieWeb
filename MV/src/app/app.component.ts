cb5e4471dce8e790ab0b29b8eb2101a971155b32
Raveesha Ramitha <ramithamainframe@gmail.com>
2025-03-31 09:42:32 +0530

    Config

--- MV/src/app/app.component.ts
+++ MV/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Analytics } from "@vercel/analytics/react"
 
 @Component({
   selector: 'app-root',
